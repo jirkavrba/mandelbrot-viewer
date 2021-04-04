eac5d5600fcbe9c6b81637f741b882a1bdd61d8d
Jiří Vrba <jiri@vrba.dev>
2021-04-04 23:54:37 +0200

    Saving canvas image to file implementation (still WIP)

--- src/main/java/dev/vrba/experiments/mandelbrot/MainWindow.java
+++ src/main/java/dev/vrba/experiments/mandelbrot/MainWindow.java
@@ -5,15 +5,26 @@ import dev.vrba.experiments.mandelbrot.math.ComplexNumber;
 import dev.vrba.experiments.mandelbrot.math.MandelbrotSet;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
+import javafx.scene.SnapshotParameters;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.Slider;
+import javafx.scene.image.PixelFormat;
+import javafx.scene.image.PixelReader;
+import javafx.scene.image.WritableImage;
+import javafx.scene.image.WritablePixelFormat;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
+import javafx.stage.FileChooser;
 import javafx.util.Pair;
+
+import java.io.*;
+import java.nio.ByteBuffer;
 
 public class MainWindow {
 
@@ -23,6 +34,10 @@ public class MainWindow {
 
     private final Slider resolution;
 
+    private final Label scaleLabel = new Label();
+
+    private final Button saveButton = new Button("Save image as file");
+
     private final MandelbrotSet mandelbrot;
 
     // This is just to prevent my PC from straight up dying
@@ -30,7 +45,6 @@ public class MainWindow {
 
     private int scale = 1;
 
-    private final Label scaleLabel = new Label();
 
     private static final int MAX_SCALE = 2000;
 
@@ -43,7 +57,6 @@ public class MainWindow {
     public final static int HEIGHT = 480;
 
     private Pair<Double, Double> dragStart;
-
 
     public MainWindow() {
         this.mandelbrot = new MandelbrotSet();
@@ -66,7 +79,9 @@ public class MainWindow {
                         this.resolution,
 
                         new Label("Scale"),
-                        this.scaleLabel
+                        this.scaleLabel,
+
+                        this.saveButton
                 ),
                 this.canvas
         );
@@ -83,6 +98,7 @@ public class MainWindow {
         this.iterations.setBlockIncrement(5);
         this.resolution.setBlockIncrement(1);
         this.scaleLabel.setFont(new Font(20));
+        this.scaleLabel.setPadding(new Insets(0, 0, 100, 0));
     }
 
     private void registerEventListeners() {
@@ -144,6 +160,47 @@ public class MainWindow {
             );
 
             this.draw();
+        });
+
+        this.saveButton.setOnMouseClicked(event -> {
+            try {
+                WritableImage image = new WritableImage(WIDTH, HEIGHT);
+                image = this.canvas.snapshot(
+                        new SnapshotParameters(),
+                        image
+                );
+
+                File target = (new FileChooser()).showSaveDialog(null);
+
+                // https://stackoverflow.com/questions/27054672/writing-javafx-scene-image-image-to-file
+                // Was to lazy to look for a better solution
+                if (target.canWrite() && !target.isDirectory()) {
+                    int width = (int) image.getWidth();
+                    int height = (int) image.getHeight();
+
+                    byte[] buffer = new byte[width * height * 4];
+
+                    PixelReader reader = image.getPixelReader();
+                    WritablePixelFormat<ByteBuffer> format = PixelFormat.getByteBgraInstance();
+
+                    reader.getPixels(0, 0, width, height, format, buffer, 0, width * 4);
+
+                    BufferedOutputStream out = new BufferedOutputStream(new FileOutputStream(target));
+
+                    for(int count = 0; count < buffer.length; count += 4) {
+                        out.write(buffer[count + 2]);
+                        out.write(buffer[count + 1]);
+                        out.write(buffer[count]);
+                        out.write(buffer[count + 3]);
+                    }
+
+                    out.flush();
+                    out.close();
+                }
+            } catch (Exception exception) {
+                Alert alert = new Alert(Alert.AlertType.ERROR, exception.getMessage());
+                alert.show();
+            }
         });
     }
 
