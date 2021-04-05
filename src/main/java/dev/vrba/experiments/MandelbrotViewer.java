faec1fb50a3246fc702fbd75299d9b2520e85e08
Jiří Vrba <jiri@vrba.dev>
2021-04-05 10:31:37 +0200

    Formatting

--- src/main/java/dev/vrba/experiments/MandelbrotViewer.java
+++ src/main/java/dev/vrba/experiments/MandelbrotViewer.java
@@ -2,8 +2,6 @@ package dev.vrba.experiments;
 
 public class MandelbrotViewer {
     public static void main(String[] arguments) {
-        MainWindow window = new MainWindow();
-
-        window.show();
+        new MainWindow().show();
     }
 }
