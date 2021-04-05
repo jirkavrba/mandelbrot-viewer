920618fc1aca971446bd965e27434903c9b6f08b
Jiří Vrba <jiri@vrba.dev>
2021-04-05 10:28:54 +0200

    Added sample window code from lwjgl website

---
+++ src/main/java/dev/vrba/experiments/MandelbrotViewer.java
@@ -0,0 +1,9 @@
+package dev.vrba.experiments;
+
+public class MandelbrotViewer {
+    public static void main(String[] arguments) {
+        MainWindow window = new MainWindow();
+
+        window.show();
+    }
+}
