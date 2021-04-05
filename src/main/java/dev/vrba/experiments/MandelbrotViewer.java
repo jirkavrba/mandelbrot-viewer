5114420d65c8ecb38fa83560a771cf5e0fbe75a2
Jiří Vrba <jiri@vrba.dev>
2021-04-05 11:50:03 +0200

    Removed LWJGL and instead use JMonkeyEngine

--- src/main/java/dev/vrba/experiments/MandelbrotViewer.java
+++ src/main/java/dev/vrba/experiments/MandelbrotViewer.java
@@ -1,7 +1,29 @@
 package dev.vrba.experiments;
 
-public class MandelbrotViewer {
+import com.jme3.app.SimpleApplication;
+import com.jme3.system.AppSettings;
+
+public class MandelbrotViewer extends SimpleApplication {
+
     public static void main(String[] arguments) {
-        new MainWindow().show();
+        MandelbrotViewer app = new MandelbrotViewer();
+
+        AppSettings settings = new AppSettings(true);
+
+        settings.setTitle("Mandelbrot viewer @jirkavrba");
+        settings.setWidth(640);
+        settings.setHeight(480);
+        settings.setFullscreen(false);
+        settings.setResizable(false);
+
+        app.setSettings(settings);
+        app.setShowSettings(false);
+        app.setDisplayFps(true);
+        app.start();
+    }
+
+    @Override
+    public void simpleInitApp() {
+
     }
 }
